6a126aab95ff7ae9113bb9f5d112f251d2ca0920
Alfonso Galaviz <agalaviz@hrhgdl.com>
2021-07-06 14:58:45 -0500

    Actualizacion Api

--- main.py
+++ main.py
@@ -17,9 +17,17 @@ users = [
 
 @app.route('/users', methods=["GET"])
 def getAllUsers():
-    return jsonify(users)
+    return jsonify(users), 200
 
-    
+
+@app.route('/users/<string:username>', methods=["GET"])
+def getUsersByUsername(username):
+    result = next((user for user in users if user["Username"] == username), None)
+    if result is not None:
+        return jsonify(result), 200
+    else:
+        return "User not found", 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
